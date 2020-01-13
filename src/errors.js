be252fd857e85f85179bc6eebbafbef5864a1a4e
IC-Tech <ic.imesh.chamara@gmail.com>
2020-01-14 04:13:50 +0530

    Update errors.js

--- src/errors.js
+++ src/errors.js
@@ -2,7 +2,9 @@
 const config = require('../config.json')
 exports.getString = a => {
 	var b = ({
-		'50013': `<@${config.megumi}> haven't permissions for that.`
+		'50013': `<@${config.megumi}> haven't permissions for that.`,
+		'10003': `Requested Channel can't be found.`
 	})[a]
 	return b ? b : 'Something went wrong, please try again later or contact the developer (type M.about).'
 }
+exports.notBugs = [50013, 10003, 50001]
